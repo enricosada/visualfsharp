439aefc13888036121ba630072a6bf774467ee32
Enrico Sada <enrico@sada.io>
2015-12-14 13:21:34 +0100

    add travisci

---
+++ build.sh
@@ -0,0 +1,7 @@
+// FROM http://docs.asp.net/en/latest/getting-started/installing-on-linux.html
+
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+
+dnvm version
+
+dnvm upgrade -r coreclr
